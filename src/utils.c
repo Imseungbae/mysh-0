217d9cb8606faeb1397acc1a96b0338c8dfc9f38
Imseungbae <tmdqo2013@naver.com>
2017-10-09 20:19:32 +0900

    sorry

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,39 @@
 #include "utils.h"
-
+#include "stdlib.h"
+#include "string.h"
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+ int cur=0;
+ int count=0;
+ char str[20];
+ char* temp;
+ char* commandsave;
+ argc=&count; 
+ strcpy(str,command);
+ strcpy(commandsave,str);
+
+ temp=strtok(str," ");
+ while(temp!=NULL){
+ count++;
+ temp=strtok(NULL," ");
+ }
+ strcpy(str,commandsave); 
+ temp=NULL;
+
+ argv=(char***)malloc(sizeof(char*)*count);
+ 
+ for(int i=0; i<count; i++){
+ argv[i]=(char**)malloc(sizeof(char*));
+ } 
+ for(int i=0; i<count; i++){
+ argv[i][0]=(char*)malloc(sizeof(char*)*10);
+ }
+ 
+ temp=strtok(str," ");
+ while(temp!=NULL){
+ strcpy(argv[cur][0],temp);
+ cur++;
+ temp=strtok(NULL," ");
+ }
 }
