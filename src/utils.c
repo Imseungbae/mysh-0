6c1797a94f0635a1cea95da6f4eb8b029033bd0f
Imseungbae <tmdqo2013@naver.com>
2017-10-09 22:54:46 +0900

    testtest

--- src/utils.c
+++ src/utils.c
@@ -1,39 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
 #include "utils.h"
-#include "stdlib.h"
-#include "string.h"
+
+
+
+
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
- int cur=0;
  int count=0;
+ char* result;
  char str[20];
- char* temp;
- char* commandsave;
- argc=&count; 
+ *argc =0;
+ char* tok=" \n\t";
+
+
  strcpy(str,command);
- strcpy(commandsave,str);
+ *argv=(char**)malloc(sizeof(char*)*20);
+ (*argv)[count]=(char*)malloc(sizeof(char)*20);
+ 
+ result=strtok(str,tok);
+ if(result==NULL){
+ strcpy((*argv)[count]," ");
+ *argc=1;
+ return;
+ }
 
- temp=strtok(str," ");
- while(temp!=NULL){
+ while(result!=NULL){
+ strcpy((*argv)[count],result);
  count++;
- temp=strtok(NULL," ");
- }
- strcpy(str,commandsave); 
- temp=NULL;
-
- argv=(char***)malloc(sizeof(char*)*count);
- 
- for(int i=0; i<count; i++){
- argv[i]=(char**)malloc(sizeof(char*));
- } 
- for(int i=0; i<count; i++){
- argv[i][0]=(char*)malloc(sizeof(char*)*10);
- }
- 
- temp=strtok(str," ");
- while(temp!=NULL){
- strcpy(argv[cur][0],temp);
- cur++;
- temp=strtok(NULL," ");
+ (*argv)[count]=(char*)malloc(sizeof(char)*20);
+ result=strtok(NULL,tok);
+ *(argc)++;
  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
